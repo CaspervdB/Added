a597f217db3914f34d80594b6e8774db69977e92
Rik2001 <rikvanrijn2001@gmail.com>
2020-01-15 10:53:38 +0100

    Revert "kleine wijzigingen zoals onnodige variabele weghalen."
    This reverts commit f2649a7f18bfcbfd9f87baedb9bfd848ba226239.

--- arduino/test_code/Test_Steppermotor/Test_Steppermotor.ino
+++ arduino/test_code/Test_Steppermotor/Test_Steppermotor.ino
@@ -11,9 +11,9 @@
 volatile char  CW =             HIGH;                                                                   // CW, true clockwise  
 volatile char CCW =             LOW;                                                                    // CCW, false counter clockwis 
 
+int hulpje = 5;
 bool X_Max = false;                                                                                      //De startrichting voor de motor van de X-as
 bool Y_Max = false;                                                                                      //De startrichting voor de motor van de Y-as
-
 int CoordinatesTotal = 4;                                                                                //Totaal aantal coordinaten
 int Coordinates[] = {5, 6, 1, 3, 7, 2, 1, 1};                                                            //De coordinaten X, Y, X, Y
 
@@ -33,19 +33,18 @@ void setup () {                                                                 
     digitalWrite (ENABLE_MOTORS, HIGH);                                                                   // LOW zet de motor aan en HIGH zet hem uit
     digitalWrite (ENABLE_MOTORS, LOW);
 
-    //runMotor("Motor_X", CCW, 200);                                                                      //Uitcommenten als je de X motor handmatig wilt aanzetten. Direction moet wel even veranderd worden.
-    //runMotor("Motor_Y", CCW, 200);                                                                      //Uitcommenten als je de Y motor handmatig wilt aanzetten. Direction moet wel even veranderd worden.
+    //runMotor("Motor_X", CCW, 200);
+    //runMotor("Motor_Y", CCW, 200);
 
-    GoHome();                                                                                             //Go to start position. coordinate (0,0).
+    GoHome();                                                                                           //Go to start position
 
-    delay(1000);
+    delay(10000);
     
     Serial.begin(9600); 
 }
 
 void loop () {
-  MoveTo(Coordinates);                                                                                    //beweegt naar de coordinaten die in de Coordinates[] array staan.
-  delay(1000);
+  MoveTo(Coordinates);
 } 
 
 /*
@@ -72,7 +71,7 @@ void Turn_X_Axis()
 
 void X_AxisDirectionChange()
 {
-  if(digitalRead(MOTOR_X_END_STOP) == HIGH)                                                              //When END_STOP is HIGH the X-Axis has reached its max
+  if(digitalRead(MOTOR_Y_END_STOP) == HIGH)                                                              //When END_STOP is HIGH the X-Axis has reached its max
   {
     runMotor("Motor_X", CCW, 0);                                                                         //Stops the X-axis from moving
     Serial.println("De X-as is op een maximale waarde");
@@ -133,7 +132,7 @@ void MoveTo(int Coordinates[])                                                  
   Serial.print(",");
   Serial.println(Coordinates[1]);
 
-  delay(1000);
+  delay(10000);
   
   for (int i = 0; i <= CoordinatesTotal; i = i+2)
   {
@@ -143,8 +142,6 @@ void MoveTo(int Coordinates[])                                                  
     Serial.println(MoveY);
     
     MoveToCoordinate(MoveX, MoveY);
-
-    delay(1000);
     
     Serial.print("Ik ben op: ");
     Serial.print(Coordinates[i+2]);
